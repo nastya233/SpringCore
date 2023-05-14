6edcadce1f3ea34c0b9be86b70e013e493db7001
Anastasya <egorycevaanastasia80@gmail.com>
2023-05-15 00:09:05 +0300

    Добавление суеещк и списка

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,14 +1,49 @@
 package org.example;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-//IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
+
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public  void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    //IoC
+
+    public MusicPlayer() {
+
+    }
+
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -12,7 +12,7 @@ public class TestSpring {
         // MusicPlayer musicPlayer = new MusicPlayer(music);
 
        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        context.close();
+       musicPlayer.playMusicList();
+       context.close();
     }
 }
